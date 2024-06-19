c3bb5827b85bc9318ee009262f1ad6177f57445a
Salar Pro <salarkhalid97@gmail.com>
2024-06-19 03:53:51 +0300

    Initial Commit

---
+++ testo2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testo2
+//
+//  Created by Salar Pro on 6/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testo2/testo2App.swift
@@ -0,0 +1,17 @@
+//
+//  testo2App.swift
+//  testo2
+//
+//  Created by Salar Pro on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct testo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
